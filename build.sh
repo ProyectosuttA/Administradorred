469939c148a12c39eb4ae501fcec7127db904e70
alejandro <alejandro@kali>
2023-03-25 01:51:03 -0600

    prueba3

--- build.sh
+++ build.sh
@@ -3,11 +3,6 @@
 set -o errexit
 
 #poetry install
-apt install postgresql
-systemctl enable postgresql
-systemctl start postgresql
-netstat -ltpn
-ls  
 
 python manage.py collectstatic --no-input
 python manage.py migrate
